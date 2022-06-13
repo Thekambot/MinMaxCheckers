64c3fef5a1cf938ed3d44b921ce79c39b3cd4f51
KamilSzus <69347040+KamilSzus@users.noreply.github.com>
2022-06-13 23:56:38 +0200

    Add files via upload

---
+++ Minimax-Checkers/scripts/pawn.gd
@@ -0,0 +1,12 @@
+extends Node
+
+onready var sprite = $AnimatedSprite
+var is_queen = false
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if "player" in self.name:
+			if not Global.lock_active:
+				Global.active = self.name
+
+
