3f89ca16b8fc1d039f399e259ef32deedf05c26d
KamilSzus <69347040+KamilSzus@users.noreply.github.com>
2022-06-13 18:46:39 +0200

    bot

--- Scripts/Pawn.gd
+++ Scripts/Pawn.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var sprite = $AnimatedSprite
+var is_queen = false
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if "player" in self.name:
+			pass
